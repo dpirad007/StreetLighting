7715abc706af536a86a125dffbb2076cdc60ae46
Dion Pinto <dionpinto000@gmail.com>
2021-04-13 12:50:22 +0530

    dependency removed

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 require("./db/mongoose");
 const LightRouter = require("./router/light");
-const cors = require("cors");
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+
 const port = 5000;
 
 app.use(LightRouter);
